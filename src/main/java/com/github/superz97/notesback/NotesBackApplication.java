e4f8351ba32b3c80cd459d91c632233f2368a29d
Zed <b.zangar15@gmail.com>
2026-08-29 11:02:29 +0500

    initial commit

---
+++ src/main/java/com/github/superz97/notesback/NotesBackApplication.java
@@ -0,0 +1,13 @@
+package com.github.superz97.notesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/superz97/notesback/NotesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.superz97.notesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
